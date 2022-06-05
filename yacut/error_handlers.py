fce649223c990fc1c5a993b564c06c0468bf9b3e
Alexandra <aleksandra.alexand2017@yandex.ru>
2022-06-05 15:36:07 +0300

    api

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -5,12 +5,12 @@ from flask import jsonify, render_template
 from . import app, db
 
 
-@app.errorhandler(HTTPStatus.BAD_REQUEST.value)
+@app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), HTTPStatus.BAD_REQUEST
 
 
-@app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR.value)
+@app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
